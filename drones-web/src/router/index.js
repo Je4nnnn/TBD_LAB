eb15f330c91a7eba292666c79124305183db93d4
Ian Rioseco <ian.rioseco@usach.cl>
2025-09-28 21:19:40 -0300

    Cambios en el dashboard y mejoras

--- drones-web/src/router/index.js
+++ drones-web/src/router/index.js
@@ -6,7 +6,10 @@ import Dashboard from '../views/DashboardView.vue'
 const routes = [
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: () => import('../views/RegisterView.vue') },
-  { path: '/', name: 'dashboard', component: Dashboard, meta: { requiresAuth: true } }
+  { path: '/', name: 'dashboard', component: Dashboard, meta: { requiresAuth: true } },
+  { path: '/misiones', name: 'misiones', component: () => import('../pages/Misiones.vue') },
+  { path: '/reportes', name: 'reportes', component: () => import('../pages/Reportes.vue') },
+  { path: '/drones', name: 'drones', component: () => import('../pages/Drones.vue') }
 ]
 
 const router = createRouter({
